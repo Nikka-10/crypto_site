b73af9912f06e8c78db607ec6a302eb2806dcbb0
Hirtai <levanibeco@gmail.com>
2025-05-20 19:39:32 +0400

    Api uses variables now

--- getAPI.py
+++ getAPI.py
@@ -1,24 +1,33 @@
 import requests
 import time
-url = 'https://api.coingecko.com/api/v3/simple/price'
-params = {  
-         'ids': 'bitcoin,ethereum',
-         'vs_currencies': 'USD'
-}
 
-headers = { 'x-cg-demo-api-key': 'CG-Um1wPk1y9NhixNZxRh5jb2vz' }
+def priceAPIcall(crypto_list, vs_currency):
+    url = 'https://api.coingecko.com/api/v3/simple/price'
+    params = {
+        'ids': ','.join(crypto_list),
+        'vs_currencies': vs_currency
+    }
+    headers = { 'x-cg-demo-api-key': 'CG-Um1wPk1y9NhixNZxRh5jb2vz' }
 
-response = requests.get(url, params = params)
-def priceAPIcall():
+    response = requests.get(url, params=params, headers=headers)
+    
     if response.status_code == 200:
         data = response.json()
-        Bitcoin_price = data['bitcoin']['usd']
-        Ethereum_price = data['ethereum']['usd']
-        print(f'The price of Ethereum in USD is ${Ethereum_price}')
-        print(f'The price of Bitcoin in USD is ${Bitcoin_price}')
+        for coin in crypto_list:
+            coin_id = coin.lower()
+            if coin_id in data:
+                price = data[coin_id][vs_currency.lower()]
+                print(f'The price of {coin.title()} in {vs_currency.upper()} is ${price}')
+            else:
+                print(f'{coin.title()} not found in API response.')
     else:
-        print('Failed to retrieve data from the API')
+        print('Failed to retrieve data from the API:', response.status_code)
+
+crypto_input = input("Enter the crypto currencies you want to check (comma-separated): ")
+vs_currency = input("Enter the currency you want to check in: ")
+
+crypto_list = [coin.strip().lower() for coin in crypto_input.split(",")]
 
 while True:
-    priceAPIcall()
+    priceAPIcall(crypto_list, vs_currency)
     time.sleep(20)
