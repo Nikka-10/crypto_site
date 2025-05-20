da7abd276c63cb51358e19094bfd360559ac4fad
Nika <nikagvasalia78@gmail.com>
2025-05-20 22:56:37 +0400

    create script that add user info into database

--- main.py
+++ main.py
@@ -2,16 +2,39 @@ import pyodbc
 import requests
 
 
-class user():
-    def __init__(self, Firstname, Lastname, email, password):
-        pass
-        
+class database():
+    
+    def __init__(self, server = 'NIKA', database = 'crypto_db'):
+        self.connection_str = f"""
+            driver={{ODBC driver 18 for SQL Server}};
+            server={server};
+            database={database};
+            TrustServerCertificate=Yes;
+            Trusted_Connection=yes;;
+            """.strip()
+            
+    def get_connection(self):    
+        return self.connection_str
+
+
+class User():
+    def __init__(self, connection_str):
+        self.connection_str = connection_str
+
+    
+    def add_user(self, Firstname, Lastname, email, password):
+        with pyodbc.connect(self.connection_str) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"insert into user_info(fname, lname, email, password_) values(?,?,?,?)", (Firstname, Lastname, email, password))
+            
+            cursor.close()
+    
 
 class crypto_operatinos():
     def __init__(self, connection):
         self.connection = connection
          
-    def Get_crypto(self,):#ვფიქრობ კრიპტოს საიტიდან აღების ფუნქციას რომ გავაკეთებთ აქ ჩასმა შეიძლება და სახელი, ფასის, ცვლილებების და სხვა ინფოს შენახვა
+    def Get_crypto(self,):
         ...
     
     def buy_crypto(self,):
@@ -24,24 +47,25 @@ class crypto_operatinos():
         ...
         
   
-    
-class database():
-    
-    def __init__(self, server = 'NIKA', database = 'crypto_base'): # საბოლოოდ რაიმე ღია მონაცემთა ბაზაზე შეიცვლება
-        self.connection_string = f"""
-            driver={{ODBC driver 18 for SQL Server}};
-            server={server};
-            database={database};
-            TrustServerCertificate=yes;
-            Trusted_Connection=yes
-            """
-        self.connection = pyodbc.connect(self.connection_string)
-            
-
-    
+     
 def main():
     db = database()
-    crupto_op = crypto_operatinos(db.connection)
+    connection_str = db.get_connection()
+    
+    user = User(connection_str)
+    crupto_op = crypto_operatinos(connection_str)
+    
+    fname, lname, email, password = get_user_info()
+    user.add_user(fname, lname, email, password)
+  
+    
+def get_user_info():
+    fname = input("wirte your first name: ")    
+    lname = input("wirte your last name: ")    
+    email = input("wirte your email address: ")    
+    password = input("wirte password: ")
+    
+    return fname, lname, email, password    
     
     
 if __name__ == "__main__":
